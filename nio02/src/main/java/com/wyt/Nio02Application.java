d03c3aad4a68fe7e8aa80cbc037daf7e5a0aab7b
wenyt <522490726@qq.com>
2021-09-28 00:28:24 +0800

    nio

---
+++ nio02/src/main/java/com/wyt/Nio02Application.java
@@ -0,0 +1,13 @@
+package com.wyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nio02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nio02Application.class, args);
+    }
+
+}
---
+++ nio02/src/test/java/com/wyt/Nio02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nio02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
